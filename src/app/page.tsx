580ef27af503b89c2d2c6abd63c92cb36016a50f
bygentltd <bygentltd@gmail.com>
2026-06-07 21:06:44 +0530

    safari webkit issue fix - VI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,7 @@ export default function Home() {
         <StatsSection />
         <FeaturedProductSection />
         <ImageSectionComp src="/HOME_21.webp" />
+        <ImageSectionComp src="/Home-Ing.webp" />
         <ImageSectionComp src="/HOME_HOW.webp" />
         {/* <MajorFeaturesSection/>
         <BentoGridSection /> */}
